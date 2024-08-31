dc3651d6256ec7165f3335d1012641291e77e105
MUHAMMAD HASEEB <haseeb03320@gmail.com>
2024-08-31 13:19:20 +0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/users');
+require('dotenv').config();
+
+const authenticateUser = async (req, res, next) => {
+    // ... (authentication logic)
+};
+
+module.exports = {
+    authenticateUser,
+};
---
+++ blogRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const controller = require('../controllers/blogController');
+const auth = require('../middlewares/auth');
+
+const blogRouter = express.Router();
+
+blogRouter.use(auth.authenticateUser); // Requires authentication for all routes
+
+blogRouter.post('/create', controller.createBlog);
+blogRouter.get('/', controller.getAllBlogs);
+blogRouter.get('/:_id', controller.getOneBlog);
+blogRouter.put('/update/:_id', controller.updateBlog);
+blogRouter.delete('/:_id', controller.deleteBlog);
+
+module.exports = blogRouter;
